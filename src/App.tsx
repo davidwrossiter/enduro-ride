8247d83507f572b4b3f1bd269d46445f36b92f63
davidwrossiter <davewrossiter@gmail.com>
2024-08-20 23:04:20 +0100

    add product

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
+import Product from "./pages/Product";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/products" element={<Products />} />
+      <Route path="/products/:productId" element={<Product />} />
     </Routes>
   );
 }
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -10,47 +10,44 @@ function Products() {
           <h4 className="font-bold text-[28px] leading-[28px]">Shop Enduro</h4>
           <div className="flex flex-col mt-8 gap-10 md:gap-4 md:flex-row">
             <div className="flex flex-col items-start">
-              <img src="./Home/enduro-seal-3.png" draggable="false" />
+              <img src="/Home/enduro-seal-3.png" draggable="false" />
               <div className="mt-5">
                 <p className="font-semibold leading-4">MTB Tire Seal</p>
                 <p className="mt-2 font-light text-sm leading-[19.6px]">
                   Premium tubeless sealant for mountain bikes.
                 </p>
-                <a
-                  href="#"
-                  className="text-[#E72323] mt-3 text-sm leading-[14px]"
-                >
-                  See more
+                <a href="/products/1">
+                  <p className="text-[#E72323] mt-3 text-sm leading-[14px]">
+                    Purchase
+                  </p>
                 </a>
               </div>
             </div>
             <div className="flex flex-col items-start">
-              <img src="./Home/enduro-seal-1.png" draggable="false" />
+              <img src="/Home/enduro-seal-1.png" draggable="false" />
               <div className="mt-5">
                 <p className="font-semibold leading-4">Road Bike Tire Seal</p>
                 <p className="mt-2 font-light text-sm leading-[19.6px]">
                   Premium tubeless sealant for road and gravel bikes.
                 </p>
-                <a
-                  href="#"
-                  className="text-[#E72323] mt-3 text-sm leading-[14px]"
-                >
-                  See more
+                <a href="/products/2">
+                  <p className="text-[#E72323] mt-3 text-sm leading-[14px]">
+                    Purchase
+                  </p>
                 </a>
               </div>
             </div>
             <div className="flex flex-col items-start">
-              <img src="./Home/enduro-seal-2.png" draggable="false" />
+              <img src="/Home/enduro-seal-2.png" draggable="false" />
               <div className="mt-5">
                 <p className="font-semibold leading-4">MTB Tire Seal</p>
                 <p className="mt-2 font-light text-sm leading-[19.6px]">
                   Premium tubeless sealant for mountain bikes.
                 </p>
-                <a
-                  href="#"
-                  className="text-[#E72323] mt-3 text-sm leading-[14px]"
-                >
-                  See more
+                <a href="products/3">
+                  <p className="text-[#E72323] mt-3 text-sm leading-[14px]">
+                    Purchase
+                  </p>
                 </a>
               </div>
             </div>
