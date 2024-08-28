4446ffa4b08eed9bae3e044b51cb1f4dab9d6876
davidwrossiter <davewrossiter@gmail.com>
2024-08-28 22:25:57 +0100

    adjust product page

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -5,11 +5,13 @@ function Products() {
   return (
     <div className="flex flex-col h-screen justify-between">
       <Header />
-      <div className="pt-[144px] pb-20 mx-7 flex flex-col items-center">
+      <div className="pt-[144px] pb-20 md:py-[200px] mx-7 flex flex-col items-center">
         <div className="w-full max-w-[1230px]">
-          <h4 className="font-bold text-[28px] leading-[28px]">Shop Enduro</h4>
-          <div className="flex flex-col mt-8 gap-10 md:gap-4 md:flex-row">
-            <div className="flex flex-col items-start">
+          <h4 className="font-bold text-[28px] leading-[28px] md:text-[48px] leading-[48px]">
+            Shop Enduro
+          </h4>
+          <div className="flex flex-col mt-8 md:flex-row md:flex-wrap">
+            <div className="flex flex-col items-start md:w-1/2 p-2">
               <img src="/Home/enduro-seal-3.png" draggable="false" />
               <div className="mt-5">
                 <p className="font-semibold leading-4">MTB Tire Seal</p>
@@ -23,8 +25,8 @@ function Products() {
                 </a>
               </div>
             </div>
-            <div className="flex flex-col items-start">
-              <img src="/Home/enduro-seal-1.png" draggable="false" />
+            <div className="flex flex-col items-start md:w-1/2 p-2">
+              <img src="/Home/enduro-seal-4.png" draggable="false" />
               <div className="mt-5">
                 <p className="font-semibold leading-4">Road Bike Tire Seal</p>
                 <p className="mt-2 font-light text-sm leading-[19.6px]">
@@ -37,8 +39,22 @@ function Products() {
                 </a>
               </div>
             </div>
-            <div className="flex flex-col items-start">
+            <div className="flex flex-col items-start md:mt-6 md:w-1/2 p-2">
               <img src="/Home/enduro-seal-2.png" draggable="false" />
+              <div className="mt-5">
+                <p className="font-semibold leading-4">MTB Tire Seal</p>
+                <p className="mt-2 font-light text-sm leading-[19.6px]">
+                  Premium tubeless sealant for mountain bikes.
+                </p>
+                <a href="products/3">
+                  <p className="text-[#E72323] mt-3 text-sm leading-[14px]">
+                    Purchase
+                  </p>
+                </a>
+              </div>
+            </div>
+            <div className="flex flex-col items-start md:mt-6 md:w-1/2 p-2">
+              <img src="/Home/enduro-seal-5.png" draggable="false" />
               <div className="mt-5">
                 <p className="font-semibold leading-4">MTB Tire Seal</p>
                 <p className="mt-2 font-light text-sm leading-[19.6px]">
